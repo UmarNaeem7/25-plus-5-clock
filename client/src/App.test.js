46c20805d2864ace3114baabe2b27a4e8df656da
AggieWB <usmaggie23@gmail.com>
2020-10-23 12:17:04 -0700

    Setup config file for Jest/Enzyme testing
    Import enzyme methods, and add App smoke test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// https://enzymejs.github.io/enzyme/
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+
+//https://jestjs.io/docs/en/expect
+//shallow: https://enzymejs.github.io/enzyme/docs/api/shallow.html
+//mount: https://enzymejs.github.io/enzyme/docs/api/mount.html
+
+it('App deeply renders as a smoke test', () => {
+  mount(<App />);
+});
