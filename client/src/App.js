6c26c0d02bf893e9b3f717feae9dfe5160862762
AggieWB <usmaggie23@gmail.com>
2020-10-26 16:06:34 -0700

    Add App class methods leftPad() and countDown()
    Add countDown() as a prop to Timer component to start onClick of start_stop button
    Reformat reset setState argument
    Add TODO for countDown() method

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,11 @@ class App extends React.Component {
         session: 25
       },
       isSession: true,
+      time: '25:00'
     }
     this.intervalTypes = Object.keys(this.state.intervals);
     this.handleChange = this.handleChange.bind(this);
+    this.countDown = this.countDown.bind(this);
     this.reset = this.reset.bind(this);
   }
 
@@ -34,19 +36,50 @@ class App extends React.Component {
     }
   }
 
+  leftPad(value){
+    if(value > 9){
+      return value + '';
+    } else {
+      return '0' + value;
+    }
+  }
+  
+  //TODO: Need to toggle and reset between session and break, and stop timer from the point the time was paused
+  countDown(){
+    let minutes = this.state.intervals.session;
+    this.setState({time: `${this.leftPad(minutes)}:00`});
+    let seconds = 59;
+    minutes--;
+    
+    const interval = setInterval(() => {
+      this.setState({time: `${this.leftPad(minutes)}:${this.leftPad(seconds)}`});
+      seconds--;
+      if(seconds < 0){
+        if(minutes > 0){
+          seconds = 59;
+          minutes--;
+        } else {
+          clearInterval(interval);
+        }
+      }
+    }, 1000);
+  }
+
   //TODO: When I click the element with the id of "reset", any running timer should be stopped and the element with id="time-left" should reset to its default state.
   reset(){
-    this.setState({intervals: {
-      break: 5,
-      session: 25
-    }});
+    this.setState({
+      intervals: {
+        break: 5,
+        session: 25
+      }
+    });
   }
   
   render(){
     return <div>
       <h1>25 + 5 Clock</h1>
       {this.intervalTypes.map(type => <IntervalController key={type} type={type} length={this.state.intervals[type]} handleChange={this.handleChange}/>)}
-      <Timer interval={this.state.isSession ? this.intervalTypes[1]: this.intervalTypes[0]} reset={this.reset}/>
+      <Timer interval={this.state.isSession ? this.intervalTypes[1]: this.intervalTypes[0]} reset={this.reset} time={this.state.time} start={this.countDown}/>
       <Footer />
     </div>;
   }
