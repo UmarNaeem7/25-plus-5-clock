daf081493eceec7dcceab0ca12534189c6a108c8
AggieWB <usmaggie23@gmail.com>
2020-10-27 21:50:02 -0700

    Add conditional rendering of props methods onClick which is
    either start or pause

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -4,7 +4,7 @@ function Timer(props){
     return <section id="timer">
         <h2 id="timer-label">{props.interval}</h2>
         <p id="time-left">{props.time}</p>
-        <button id="start_stop" onClick={props.start}>&#9199;</button>
+        <button id="start_stop" onClick={props.isPaused ? props.start : props.pause}>&#9199;</button>
         <button id="reset" onClick={props.reset}>&#8634;</button>
     </section>;
 }
