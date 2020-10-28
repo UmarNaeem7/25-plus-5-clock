5658425c73acfe1ac597ed5448dd5b2b5e75f9f1
AggieWB <usmaggie23@gmail.com>
2020-10-28 10:23:35 -0700

    Uninstall enzyme-adapter-react-16, and install unoffical module
    @wojtekmaj/enzyme-adapter-react-17 after enzyme was throwing an error due to not working with new React version 17

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,4 +1,4 @@
 // https://enzymejs.github.io/enzyme/
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
