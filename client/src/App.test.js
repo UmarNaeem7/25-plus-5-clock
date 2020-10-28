0a149e7f70c00b1ae6d6b2f583d9f734be5af670
AggieWB <usmaggie23@gmail.com>
2020-10-28 09:41:39 -0700

    Import components IntervalController, Timer, and Footer

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from './App';
+import IntervalController from './components/IntervalController.js';
+import Timer from './components/Timer.js';
+import Footer from './components/Footer.js';
 
 //https://jestjs.io/docs/en/expect
 //shallow: https://enzymejs.github.io/enzyme/docs/api/shallow.html
